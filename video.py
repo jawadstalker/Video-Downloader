9f8d4290e392a9f1eb98948dfe04d7d933586d43
Death stalker <133969331+jawadstalker@users.noreply.github.com>
2025-11-17 14:39:43 +0330

    Update video.py

--- video.py
+++ video.py
@@ -5,7 +5,7 @@ DOWNLOAD_FOLDER = "downloads"
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
 def get_formats(url):
-    """بازگرداندن فرمت‌های قابل دانلود ویدیوی یوتیوب"""
+    
     ydl_opts_info = {"listformats": True}
     with YoutubeDL(ydl_opts_info) as ydl:
         info = ydl.extract_info(url, download=False)
@@ -13,7 +13,7 @@ def get_formats(url):
         return [f for f in formats if f.get("height")]
 
 def download_video(url, format_id):
-    """دانلود ویدیو با فرمت انتخاب شده"""
+   
     ydl_opts_download = {
         "format": format_id,
         "outtmpl": os.path.join(DOWNLOAD_FOLDER, "video_%(height)sp.%(ext)s"),
